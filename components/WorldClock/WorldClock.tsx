cb77ab50071f8f5c904eedea2af7a96f715e52d8
lx521603 <x@glee.ch>
2025-10-18 23:30:42 +0800

    fix

--- components/WorldClock/WorldClock.tsx
+++ components/WorldClock/WorldClock.tsx
@@ -1,39 +1,39 @@
-// components/WorldClock/WorldClock.tsx
-'use client';
-
 import { Clock } from '@gfazioli/mantine-clock';
 import { Group, Stack, Text } from '@mantine/core';
 
-export function WorldClock() {
+export function WorldClock() {  // 添加 export
   return (
-    <Group grow>
-      <Stack align="center">
+    <Group justify="center" gap="xl">
+      <Stack align="center" gap="sm">
         <Clock 
-          timezone="Asia/Shanghai" 
-          size={200} 
-          showSeconds
+          timezone="Asia/Shanghai"
+          size={200}
+          secondHandBehavior="smooth"
+          primaryNumbersProps={{ c: 'blue.6', fw: 600 }}
         />
         <Text fw={600} c="dimmed">
           上海
         </Text>
       </Stack>
-
-      <Stack align="center">
+      
+      <Stack align="center" gap="sm">
         <Clock 
-          timezone="America/Los_Angeles" 
-          size={200} 
-          showSeconds
+          timezone="America/Los_Angeles"
+          size={200}
+          secondHandBehavior="smooth"
+          primaryNumbersProps={{ c: 'green.6', fw: 600 }}
         />
         <Text fw={600} c="dimmed">
           洛杉矶
         </Text>
       </Stack>
-
-      <Stack align="center">
+      
+      <Stack align="center" gap="sm">
         <Clock 
-          timezone="Europe/London" 
-          size={200} 
-          showSeconds
+          timezone="Europe/London"
+          size={200}
+          secondHandBehavior="smooth"
+          primaryNumbersProps={{ c: 'orange.6', fw: 600 }}
         />
         <Text fw={600} c="dimmed">
           伦敦
@@ -42,5 +42,3 @@ export function WorldClock() {
     </Group>
   );
 }
-
-export default WorldClock;
