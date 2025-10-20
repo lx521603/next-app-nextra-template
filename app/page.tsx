5b1bcc00732cd02db59ccea73a7d328f26a77144
lx521603 <x@glee.ch>
2025-10-21 01:42:50 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Container } from '@mantine/core';
 import { Welcome } from '@/components/Welcome/Welcome';
 import { WorldClock } from '@/components/WorldClock/WorldClock';
 import { HomeCarousel } from '@/components/Carousel/Carousel'; // 修改导入名
-import { Content } from '@/components/Content/Content';
+//import { Content } from '@/components/Content/Content';
 
 export default function HomePage() {
   return (
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Welcome />
       <HomeCarousel /> {/* 使用新名称 */}
       <WorldClock />
-      <Content />
+      {/*<Content />*/}
     </Container>
   );
 }
